433bdbbf25fb9d66d8e2ddbcbe0e3889bcee1aa3
Yannick KERMAREC <yannick@kermarec.com>
2018-09-13 23:31:08 +0200

    Create oldlgtv-send.js

---
+++ nodes/oldlgtv-send.js
@@ -0,0 +1,21 @@
+module.exports = function (RED) {
+    function OldLgtvSendNode(n) {
+        RED.nodes.createNode(this, n);
+        var node = this;
+        this.tv = n.tv;
+        this.tvConn = RED.nodes.getNode(this.tv);
+
+        if (this.tvConn) {
+            this.tvConn.register(node);
+
+            node.on('input', function (msg) {
+                if (msg.payload && node.tvConn.send) {
+                    node.tvConn.send('send', {charkey: (String(msg.payload)).toUpperCase()});
+                }
+            });
+        } else {
+            this.error('No TV Configuration');
+        }
+    }
+    RED.nodes.registerType('lgtv-button', LgtvButtonNode);
+};
